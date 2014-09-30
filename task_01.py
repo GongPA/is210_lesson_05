e02977879803b577d91df61a1d9361c4a5a5a7d0
GongPA <david.chen@spsmail.cuny.edu>
2014-09-30 13:20:40 -0400

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -4,9 +4,8 @@
 Task 01: Defining a Function
 """
 
-def bool_to_str(bvalue, short=False):   # convert bool to string
-    
-    
+def bool_to_str(bvalue, short=False):
+    """convert bool to string"""
     if bvalue is True:
         if not short:
             return 'Yes'
