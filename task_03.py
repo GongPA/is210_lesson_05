5eac6f8264edb1272da2fa075e6d8e0a9806f306
GongPA <david.chen@spsmail.cuny.edu>
2014-10-01 15:29:37 -0400

    Update task_03.py

--- task_03.py
+++ task_03.py
@@ -7,35 +7,38 @@ Task_03:Transforming Data
 
 def celsius_to_fahrenheit(temperature):
     """convert C to F"""
-    FAHRENHEIT = (temperature * 9) / 5 + 32
-    return round(float(FAHRENHEIT), 1)  # only need 1 afetr decimal
+    fahrenheit = (temperature * 9) / 5 + 32
+    return round(float(fahrenheit), 1)  # only need 1 afetr decimal
 
 
 def fahrenheit_to_celsius(temperature):
     """ convert F to C"""
-    CELSIUS = (temperature - 32)*5 / 9
-    return round(float(CELSIUS), 1)  # only need 1 afetr decimal
+    celsius = (temperature - 32)*5 / 9
+    return round(float(celsius), 1)  # only need 1 afetr decimal
 
 
 def convert_temperature(temperature, output_format='c'):
     """ Function recall functions"""
+    temp = None
+    degree_type = None
+    
     if isinstance(temperature, basestring):
-        TEMP = float(temperature[0:-1])
-        DEGREE_TYPE = temperature[-1].lower()
+        temp = float(temperature[0:-1])
+        degree_type = temperature[-1].lower()
     else:
         return None
 
-    if iutput_format == 'f':
-        if DEGREE_TYPE == 'c':
-            return celsius_to_fahrenheit(TEMP)
-        elif DEGREE_TYPE == 'f':
-            return TEMP
+    if output_format == 'f':
+        if degree_type == 'c':
+            return celsius_to_fahrenheit(temp)
+        elif degree_type == 'f':
+            return temp
         else:
             return None
     elif output_format == 'c':
-        if DEGREE_TYPE == 'f':
-            return fahrenheit_to_celsius(TEMP)
-        elif DEGREE_TYPE == 'c':
-            return TEMP
+        if degree_type == 'f':
+            return fahrenheit_to_celsius(temp)
+        elif degree_type == 'c':
+            return temp
         else:
             return None
